d0b6300c8d894d54c8c4e59b05b5f314b447b20f
Igor Sinitsyn <web.gas.sin@gmail.com>
2017-10-23 15:48:33 +0300

    Adds small_file_name, order, description

--- module/IodogsDoctrine/src/IodogsDoctrine/Entity/ImageStorage.php
+++ module/IodogsDoctrine/src/IodogsDoctrine/Entity/ImageStorage.php
@@ -3,6 +3,7 @@
 namespace IodogsDoctrine\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Id\UuidGenerator;
 
 /**
  * FileStorage
@@ -15,18 +16,39 @@ class ImageStorage
     /**
      * @var integer
      *
-     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Column(name="id", type="string", length=64, nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\GeneratedValue(strategy="UUID")
      */
     private $id;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="file_path", type="string", length=200, nullable=false)
+     * @ORM\Column(name="s3_file_path", type="string", length=256, nullable=false)
      */
-    private $filePath;
+    private $s3FilePath;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="s3_small_file_path", type="string", length=256, nullable=false)
+     */
+    private $s3SmallFilePath;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="sort_order", type="integer", nullable=true, options={"default" = 100})
+     */
+    private $sortOrder = 100;
 
     /**
      * @var \DateTime
@@ -46,26 +68,75 @@ class ImageStorage
     }
 
     /**
-     * Set filePath
-     *
-     * @param string $filePath
+     * @return string
+     */
+    public function getS3FilePath()
+    {
+        return $this->s3FilePath;
+    }
+
+    /**
+     * @param string $s3FilePath
      * @return ImageStorage
      */
-    public function setFilePath($filePath)
+    public function setS3FilePath($s3FilePath)
     {
-        $this->filePath = $filePath;
-
+        $this->s3FilePath = $s3FilePath;
         return $this;
     }
 
     /**
-     * Get filePath
-     *
-     * @return string 
+     * @return string
      */
-    public function getFilePath()
+    public function getS3SmallFilePath()
     {
-        return $this->filePath;
+        return $this->s3SmallFilePath;
+    }
+
+    /**
+     * @param string $s3SmallFilePath
+     * @return ImageStorage
+     */
+    public function setS3SmallFilePath($s3SmallFilePath)
+    {
+        $this->s3SmallFilePath = $s3SmallFilePath;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return ImageStorage
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSortOrder()
+    {
+        return $this->sortOrder;
+    }
+
+    /**
+     * @param int $sortOrder
+     * @return ImageStorage
+     */
+    public function setSortOrder($sortOrder)
+    {
+        $this->sortOrder = $sortOrder;
+        return $this;
     }
 
     /**
