da8272a66339256061a85e60e16c5190d21dc06d
Igor Sinitsyn <web.gas.sin@gmail.com>
2017-11-17 17:15:55 +0300

    Fix breed search

--- module/IodogsApplication/src/IodogsApplication/Controller/SearchController.php
+++ module/IodogsApplication/src/IodogsApplication/Controller/SearchController.php
@@ -1,24 +1,28 @@
 <?php
 namespace IodogsApplication\Controller;
 
+use IodogsBreed\Service\BreedService;
 use Zend\Mvc\Controller\AbstractActionController,
     Zend\View\Model\ViewModel;
 
 class SearchController extends AbstractActionController
 {
+    /** @var \Psr\Container\ContainerInterface $container */
+    private $container;
 
-    public function __construct($sl)
+    /** @var \IodogsBreed\Service\BreedService $breedService */
+    private $breedService;
+
+    public function __construct($container)
     {
-        $this->sl = $sl;
+        $this->container = $container;
+        $this->breedService = $this->container->get(BreedService::class);
     }
 
     public function indexAction()
     {
-        //sleep(1);
-
-        $breedService = $this->sl->get('BreedServiceFactory');
-        $breeds = $breedService->getBreeds();
-        $breedArray = $breedService->getViewInfoByArray($breeds);
+        $breeds = $this->breedService->getBreeds();
+        $breedArray = $this->breedService->getViewInfoByArray($breeds);
 
         $query = (!empty($_GET['q'])) ? strtolower($_GET['q']) : null;
 
@@ -27,45 +31,7 @@ class SearchController extends AbstractActionController
         }
 
         $status = true;
-        $databaseUsers = array(
-            array(
-                "id"        => 1,
-                "title"  => "Тестовый заголовок",
-                "img"    => "https://avatars2.githubusercontent.com/u/4152589",
-                'slug' => 'slug'
-            ),
-        );
 
-
-      /*  $databaseProjects = array(
-            array(
-                "id"        => 1,
-                "project"   => "jQuery Typeahead",
-                "image"     => "http://www.runningcoder.org/assets/jquerytypeahead/img/jquerytypeahead-preview.jpg",
-                "version"   => "1.7.0",
-                "demo"      => 10,
-                "option"    => 23,
-                "callback"  => 6,
-            ),
-            array(
-                "id"        => 2,
-                "project"   => "jQuery Validation",
-                "image"     => "http://www.runningcoder.org/assets/jqueryvalidation/img/jqueryvalidation-preview.jpg",
-                "version"   => "1.4.0",
-                "demo"      => 11,
-                "option"    => 14,
-                "callback"  => 8,
-            )
-        );
-
-        $resultProjects = [];
-        foreach ($databaseProjects as $key => $oneProject) {
-            if (strpos(strtolower($oneProject["project"]), $query) !== false) {
-                $resultProjects[] = $oneProject;
-            }
-        }*/
-
-// Means no result were found
         if (empty($breedArray) && empty($resultProjects)) {
             $status = false;
         }
